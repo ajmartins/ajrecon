2d5c66bb626c93ac28ad4be56c4fa4f1cf6d1dca
ajmartins <ajayimartins@gmail.com>
2019-05-18 15:53:17 +0100

    Feature: Added some tools
    Linkfinder, Webscreenshot, Waybackurls, Dirsearch

--- ajrecon.py
+++ ajrecon.py
@@ -1,7 +1,7 @@
+#!/usr/bin/python
 import subprocess
 import os
 import sys
-import socket
 from datetime import date
 
 
@@ -9,7 +9,7 @@ def get_filename_datetime():
     return ' ' + str(date.today())
 
 def create_directory(domain, name):
-    subdomain_directory = 'Reports/' + domain + name + '/subdomains'
+    subdomain_directory = 'Reports/' + domain + name
     if not os.path.isdir(subdomain_directory):
         os.makedirs(subdomain_directory)
 
@@ -34,29 +34,96 @@ def subdis(domain, name):
 		  + "\n#####################################"
 		  + "\nBeginning Subdomain Enumeration. This may take a few minutes.\n")
     try:
-        ip_address = socket.gethostbyname(domain)
+        print ("\n[+] Running Sublist3r:\n")
+        subdomains_file = 'Reports/' + domain + name + '/subdomains.txt'
+        proc = subprocess.Popen(["python","Tools/Sublist3r/sublist3r.py","-d",
+                domain,"-o", subdomains_file],stdout=subprocess.PIPE)
+        sublist3rResult = handle_output(proc)
+        return {"Sublist3r": sublist3rResult}
 
-        if ip_address:
-            print ("\n[+] Running Knockpy:\n")
-            proc = subprocess.Popen(["python","Tools/knock/knockpy/knockpy.py","-c",
-                    domain],stdout=subprocess.PIPE)
-            knockpyResult = handle_output(proc)
-
-            print ("\n[+] Running Sublist3r:\n")
-            sublist3r_file = 'Reports/' + domain + name + '/subdomains/sublist3r.txt'
-            proc = subprocess.Popen(["python","Tools/Sublist3r/sublist3r.py","-d",
-                    domain,"-o", sublist3r_file],stdout=subprocess.PIPE)
-            sublist3rResult = handle_output(proc)
-
-            return {"Knockpy": knockpyResult, "Sublist3r": sublist3rResult}
     except Exception,e:
         print ("\n[!] Subdomain Discovery Failed: " + str(e) + "\n")
         return ""
 
+# Finding New Endpoints/JS Files[Domain].
+def newendpoints(domain, name):
+    print ("#####################################"
+		  + "\nFINDING NEW ENDPOINTS"
+		  + "\n#####################################"
+		  + "\nChecking for Endpoints. This may take a few minutes.\n")
+    try:
+        print ("\n[+] Running LinkFinder:\n")
+        endpoints_file = 'Reports/' + domain + name + '/endpoints.html'
+        proc = subprocess.Popen(["python","Tools/LinkFinder/linkfinder.py","-i",
+                "https://"+domain,"-d","-o",endpoints_file],stdout=subprocess.PIPE)
+        LinkFinderResult = handle_output(proc)
+
+        return {"LinkFinder": LinkFinderResult}
+    except Exception,e:
+        print ("\n[!] Finding New Endpoints Failed: " + str(e) + "\n")
+        return ""
+
+# Go Back In Time[Domain].
+def gobackintime(domain):
+    print ("#####################################"
+		  + "\nFINDING JS FILES"
+		  + "\n#####################################"
+		  + "\nChecking for JS Files. This may take a few minutes.\n")
+    try:
+        print ("\n[+] Running WayBackUrls:\n")
+        proc = subprocess.Popen(["waybackurls",domain,"| uniq | sort"],stdout=subprocess.PIPE)
+        WayBackUrlsResult = handle_output(proc)
+
+        return {"WayBackUrls": WayBackUrlsResult}
+    except Exception,e:
+        print ("\n[!] Finding JS Files Failed: " + str(e) + "\n")
+        return ""
+
+# Screenshots[Subdomains].
+def screenshots(domain, name):
+    print ("#####################################"
+		  + "\nTAKING SCREENSHOTS"
+		  + "\n#####################################"
+		  + "\nTaking Screenshots for Validation. This may take a few minutes.\n")
+    try:
+        print ("\n[+] Running WebScreenshot:\n")
+        subdomains_file = 'Reports/' + domain + name + '/subdomains.txt'
+        screenshots_files = 'Reports/' + domain + name + '/Screenshots/'
+        proc = subprocess.Popen(["python","Tools/webscreenshot/webscreenshot.py",
+                "-i",subdomains_file,"-o",screenshots_files],stdout=subprocess.PIPE)
+        WebScreenshotResult = handle_output(proc)
+
+        return {"WebScreenshot": WebScreenshotResult}
+    except Exception,e:
+        print ("\n[!]Taking Screenshots Failed: " + str(e) + "\n")
+        return ""
+
+# Content Discovery[Subdomains].
+def contentdis(domain, name):
+    print ("#####################################"
+		  + "\nCHECKING FOR COMMON FILES/DIRECTORIES"
+		  + "\n#####################################"
+		  + "\nBruteforcing for Files/Directories. This will definitely take more than a few minutes.\n")
+    try:
+        print ("\n[+] Running Dirsearch:\n")
+        subdomains_file = 'Reports/' + domain + name + '/subdomains.txt'
+        contentdis_file = 'Reports/' + domain + name + '/contentdiscovery.txt'
+        proc = subprocess.Popen(["python3","Tools/dirsearch/dirsearch.py", "-e","txt",
+                "-L",subdomains_file,"--plain-text-report="+contentdis_file],stdout=subprocess.PIPE)
+        DirsearchResult = handle_output(proc)
+
+        return {"Dirsearch": DirsearchResult}
+    except Exception,e:
+        print ("\n[!]Content Discovery Failed: " + str(e) + "\n")
+        return ""
+
+# portscan
+# PossibleVulnScan= Cors, SubdomainTakeover,AWS/Github etc.
+
 
 def main():
     print ("AJRECON")
-    print ("Bros you go like relax small\n")
+    print ("Sit back and Relax\n")
 
     if len(sys.argv) != 2:
 		print ("\n[+] Usage: %s <domain name>\n" % sys.argv[0])
@@ -67,6 +134,10 @@ def main():
 
     create_directory(domain, name)
     subdisResult = subdis(domain, name)
+    newendResult = newendpoints(domain, name)
+    jsfileResult = gobackintime(domain)
+    webscreensht = screenshots(domain, name)
+    condisResult = contentdis(domain, name)
 
 
 if __name__ == '__main__':
